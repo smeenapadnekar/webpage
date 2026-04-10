37167344f07a3bc4582001f4998b50a99b547730
smeenapadnekar <smeenapadnekar@gmail.com>
2026-04-10 11:31:18 +0000

    Deploying to gh-pages from @ smeenapadnekar/smeenapadnekar.github.io@eb5374adad86e417c4ea6148ea1c2eb9784f8b73 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "smeenapadnekar/smeena",
+    "data-repo": "smeenapadnekar/smeenapadnekar.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
